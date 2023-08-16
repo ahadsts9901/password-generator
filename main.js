706f28fa9a1745e11357c7768cb621ef4946380a
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-08-16 20:18:59 +0500

    ui

---
+++ main.js
@@ -0,0 +1,17 @@
+let slider = document.getElementById("range");
+let sliderValue = document.getElementById("passwordLength");
+let strengthText = document.getElementById("strengthText");
+let strengthBox1 = document.getElementById("strengthBox1");
+let strengthBox2 = document.getElementById("strengthBox2");
+let strengthBox3 = document.getElementById("strengthBox3");
+let strengthBox4 = document.getElementById("strengthBox4");
+
+let generateButton = document.getElementById("generateButton");
+let lowercaseCheckbox = document.getElementById("lowercaseCheckbox");
+let uppercaseCheckbox = document.getElementById("uppercaseCheckbox");
+let numbersCheckbox = document.getElementById("numbersCheckbox");
+let symbolsCheckbox = document.getElementById("symbolsCheckbox");
+
+let generatedPassword = document.getElementById("generatedPassword");
+let copyImage = document.getElementById("copyImage");
+let copyText = document.getElementById("copyText");
